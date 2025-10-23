eb2c152b4f94f162e786473ab2b96ab168b0678a
Kieran Poropat <kieran.poropat@gmail.com>
2025-10-23 13:29:14 -0400

    Remove env.dart from version control

--- lib/env.dart
+++
@@ -1,5 +0,0 @@
-class Env {
-  static const supabaseUrl = 'https://chcwwdrvzlnaygqyjhnw.supabase.co';
-  static const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9...';
-  static const youtubeApiKey = 'AIzaSyCD0wRSf8U65ld4X74WvTpH0EtZXQaGuC8';
-}
